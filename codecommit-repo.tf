e50f8b7499c5d20adfefb01c27a4aaa639979c86
Edward Allen Mercado <31796531+edwardmercado@users.noreply.github.com>
2022-08-17 15:57:21 +0800

    Update codecommit-repo.tf

--- codecommit-repo.tf
+++ codecommit-repo.tf
@@ -1,4 +1,4 @@
 resource "aws_codecommit_repository" "test" {
-  repository_name = var.repository_name
+  repository_name = var.codecommit_repository_name
   description     = "my test repo created by Terraform"
 }
